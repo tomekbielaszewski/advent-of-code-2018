3d3ddc330c98a95610faf84c05569dcdd581a02f
tbielaszewski <tomekbielaszewski@gmail.com>
2018-12-02 12:24:43 +0100

    Day 2, part 1 and 2 Python: removing unnecessary class

--- python/d1_varying_frequencies/part1.py
+++ python/d1_varying_frequencies/part1.py
@@ -6,43 +6,42 @@ ops = {
 }
 
 
-class Solution:
-    def calcFrequency(self, changes):
-        total = 0
-        for change in changes:
-            total = ops[change[0]](total, int(change[1:]))
-        return total
+def calcFrequency(changes):
+    total = 0
+    for change in changes:
+        total = ops[change[0]](total, int(change[1:]))
+    return total
 
 
 class TestSolution(unittest.TestCase):
 
     def test_example1(self):
         changes = ["+1", "-2", "+3", "+1"]
-        total = Solution().calcFrequency(changes)
+        total = calcFrequency(changes)
 
         self.assertEqual(3, total)
 
     def test_example2(self):
         changes = ["+1", "+1", "+1"]
-        total = Solution().calcFrequency(changes)
+        total = calcFrequency(changes)
 
         self.assertEqual(3, total)
 
     def test_example3(self):
         changes = ["+1", "+1", "-2"]
-        total = Solution().calcFrequency(changes)
+        total = calcFrequency(changes)
 
         self.assertEqual(0, total)
 
     def test_example4(self):
         changes = ["-1", "-2", "-3"]
-        total = Solution().calcFrequency(changes)
+        total = calcFrequency(changes)
 
         self.assertEqual(-6, total)
 
     def test_finalSolution(self):
         with open("input.txt") as f:
             changes = f.readlines()
-            total = Solution().calcFrequency(changes)
+            total = calcFrequency(changes)
 
             print(total)
