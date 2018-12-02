b9ed5a1842e0c354e3c149350bbe1e4a1fc17b77
tbielaszewski <tomekbielaszewski@gmail.com>
2018-12-02 12:37:08 +0100

    Day 2, part 1 and 2 Python: removing unnecessary class

--- python/d2_checksum_calculation/part2.py
+++ python/d2_checksum_calculation/part2.py
@@ -1,39 +1,8 @@
 import unittest
-from functools import reduce
 
 
 def calculate_checksum(ids):
-    counted_letters_in_ids = [count_letters(_id) for _id in ids]
-    doubled_char_occurrences_in_ids = filter_occurrences_in_amount_of(2, counted_letters_in_ids)
-    tripled_char_occurrences_in_ids = filter_occurrences_in_amount_of(3, counted_letters_in_ids)
-    amount_of_doubled_char_occurrences = reduce(count_if_any, doubled_char_occurrences_in_ids, 0)
-    amount_of_tripled_char_occurrences = reduce(count_if_any, tripled_char_occurrences_in_ids, 0)
-    return amount_of_doubled_char_occurrences * amount_of_tripled_char_occurrences
-
-
-def count_if_any(acc, i):
-    if i > 0:
-        acc += 1
-    return acc
-
-
-def filter_occurrences_in_amount_of(amount, counted_letters_in_ids):
-    return [count_occurrence_amounts(amount, counted_letters) for counted_letters in
-            counted_letters_in_ids]
-
-
-def count_letters(_id):
-    counted_letters = {}
-    for char in _id:
-        if char in counted_letters.keys():
-            counted_letters[char] += 1
-        else:
-            counted_letters[char] = 1
-    return counted_letters
-
-
-def count_occurrence_amounts(occurrences, counted_letters):
-    return len({k: v for (k, v) in counted_letters.items() if v == occurrences})
+    pass
 
 
 class TestSolution(unittest.TestCase):
