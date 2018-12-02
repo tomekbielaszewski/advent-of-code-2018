89b3a5c3e8769992cdaa4faee5a93e3c92e637a3
tbielaszewski <tomekbielaszewski@gmail.com>
2018-12-02 01:12:57 +0100

    Day 1, part 2 solution with tests. Searching for a first duplicate in a stream of summed up values

---
+++ src/d1_varying_frequencies/part2.py
@@ -0,0 +1,63 @@
+import unittest
+
+ops = {
+    '+': lambda n1, n2: n1 + n2,
+    '-': lambda n1, n2: n1 - n2,
+}
+
+
+class Solution:
+    def findDuplicatedFrequency(self, changes):
+        total = 0
+        already_seen = set()
+        length = len(changes)
+        counter = 0
+
+        while total not in already_seen:
+            already_seen.add(total)
+            change = changes[counter]
+            total = ops[change[0]](total, int(change[1:]))
+            counter += 1
+            if counter >= length:
+                counter = 0
+        return total
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_example1(self):
+        changes = ["+1", "-2", "+3", "+1"]
+        total = Solution().findDuplicatedFrequency(changes)
+
+        self.assertEqual(2, total)
+
+    def test_example2(self):
+        changes = ["+1", "-1"]
+        total = Solution().findDuplicatedFrequency(changes)
+
+        self.assertEqual(0, total)
+
+    def test_example3(self):
+        changes = ["+3", "+3", "+4", "-2", "-4"]
+        total = Solution().findDuplicatedFrequency(changes)
+
+        self.assertEqual(10, total)
+
+    def test_example4(self):
+        changes = ["-6", "+3", "+8", "+5", "-6"]
+        total = Solution().findDuplicatedFrequency(changes)
+
+        self.assertEqual(5, total)
+
+    def test_example5(self):
+        changes = ["+7", "+7", "-2", "-7", "-4"]
+        total = Solution().findDuplicatedFrequency(changes)
+
+        self.assertEqual(14, total)
+
+    def test_finalSolution(self):
+        with open("input.txt") as f:
+            changes = f.readlines()
+            total = Solution().findDuplicatedFrequency(changes)
+
+            print(total)
